e8474c67827aaefceb042ca01b68414f1a1bb17b
cccccroge <pooopooo543@gmail.com>
2020-01-29 15:12:37 +0800

    Before split two machanism

--- main_scene/character/MainController.gd
+++ main_scene/character/MainController.gd
@@ -1,13 +1,20 @@
-extends RigidBody2D
+extends KinematicBody2D
 
-export var velocity = 5
+export var velocity = 100
+export var gravity = 98
+export var jump = 100
 
-func _ready():
-	pass
 
+func _physics_process(delta):
+	var v := Vector2()
+	
+	v.y += gravity
+	if Input.is_action_pressed("character_move_right"):
+		v.x += velocity
+	elif Input.is_action_pressed("character_move_left"):
+		v.x -= velocity
+	elif Input.is_action_just_pressed("character_move_up"):
+		v.y -= jump
+	
+	move_and_slide(v, Vector2.UP)
 
-func _integrate_forces(state):
-	#if Input.is_action_pressed("character_move_right"):
-		var transform = state.get_transform()
-		transform.origin.x += velocity
-		state.set_transform(transform)

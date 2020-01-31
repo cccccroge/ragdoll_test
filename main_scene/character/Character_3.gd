1efe933881f09ef513075e20a1dc14e23ff6ed0c
cccccroge <pooopooo543@gmail.com>
2020-01-31 20:37:00 +0800

    Add some test

--- main_scene/character/Character_3.gd
+++ main_scene/character/Character_3.gd
@@ -1,9 +1,9 @@
 extends RigidBody2D
 
+export var SPEED = 5
+export var JUMP_SPEED = 15
 
-export var velocity = 5
-
-export var anim_ratio = 0.05
+export var anim_ratio = 0.1
 var physics_ratio = 1 - anim_ratio
 var isBlend := true
 
@@ -69,33 +69,42 @@ func _ready():
 
 
 func _physics_process(delta):
-	if not isBlend:
-		return
+	if isBlend:
+		# blend rigid's rotation with animation
+		for i in range(bone_list.size()-4):
+			var physics = rigid_list[i].get_rotation()
+			var animation = bone_list[i].get_rotation() + rot_offset_list[i]
+			rigid_list[i].set_rotation(physics * physics_ratio + animation * anim_ratio)
 	
-	# blend rigid's rotation with animation
-	for i in range(bone_list.size()):
-		var physics = rigid_list[i].get_rotation()
-		var animation = bone_list[i].get_rotation() + rot_offset_list[i]
-		rigid_list[i].set_rotation(physics * physics_ratio + animation * anim_ratio)
+	# blend feet anyway to look more realistic
+	for i in range(bone_list.size()-1, bone_list.size()-5, -1):
+			var physics = rigid_list[i].get_rotation()
+			var animation = bone_list[i].get_rotation() + rot_offset_list[i]
+			rigid_list[i].set_rotation(physics * physics_ratio + animation * anim_ratio)
 
 
 func _integrate_forces(state):
-	var xform = state.get_transform()
+	var f := Vector2()
+	isBlend = false
 	
 	if Input.is_action_pressed("character_move_right"):
-		xform.origin.x += velocity
+		f.x += SPEED
 		isBlend = true
 		anim_player.play("Run")
 	elif Input.is_action_pressed("character_move_left"):
-		xform.origin.x -= velocity
+		f.x -= SPEED
 		isBlend = true
 		anim_player.play("Run")
+		
+	elif Input.is_action_just_pressed("character_move_up"):
+		f.y -= JUMP_SPEED
+		isBlend = true
+		anim_player.play("Idle")
 	else:
 		#anim_player.play("Run")
 		anim_player.play("Idle")
 		#anim_player.stop()
-		#isBlend = false
 		pass
 	
-	state.set_transform(xform)
+	apply_impulse(Vector2(), f)
 

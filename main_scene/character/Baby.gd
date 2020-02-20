f3fa888481b560e05c25aef1c9ca015cd445694f
cccccroge <pooopooo543@gmail.com>
2020-02-21 01:08:53 +0800

    Working, using ball controller, but slippery

--- main_scene/character/Baby.gd
+++ main_scene/character/Baby.gd
@@ -15,7 +15,7 @@ var rigid_list := []
 var bone2rigid_rot_list := []	# offset to transform from bone-coord to rigid-coord
 								# TODO: it's tedious, need refactoring
 
-onready var anim_player = get_node("../AnimationPlayer")
+onready var anim_player = get_node("AnimationPlayer")
 
 
 func _ready():
@@ -49,19 +49,18 @@ func _ready():
 
 
 func _physics_process(delta):
-#	if isBlend:
-#		# blend rigid's rotation with animation
-#		for i in range(bone_list.size()):
-#			var physics = rigid_list[i].get_rotation()
-#			var animation = bone_list[i].get_rotation() + bone2rigid_rot_list[i]
-#			rigid_list[i].set_rotation(physics * physics_ratio + animation * anim_ratio)
+	if isBlend:
+		# blend rigid's rotation with animation
+		for i in range(0, bone_list.size()-4):
+			var physics = rigid_list[i].get_rotation()
+			var animation = bone_list[i].get_rotation() + bone2rigid_rot_list[i]
+			rigid_list[i].set_rotation(physics * physics_ratio + animation * anim_ratio)
 	
-#	# blend feet anyway to look more realistic
-#	for i in range(bone_list.size()-1, bone_list.size()-5, -1):
-#			var physics = rigid_list[i].get_rotation()
-#			var animation = bone_list[i].get_rotation() + bone2rigid_rot_list[i]
-#			rigid_list[i].set_rotation(physics * physics_ratio + animation * anim_ratio)
-	pass
+	# blend feet anyway to look more realistic
+	for i in range(bone_list.size()-4, bone_list.size()):
+			var physics = rigid_list[i].get_rotation()
+			var animation = bone_list[i].get_rotation() + bone2rigid_rot_list[i]
+			rigid_list[i].set_rotation(physics * physics_ratio + animation * anim_ratio)
 
 
 func _integrate_forces(state):
@@ -71,22 +70,21 @@ func _integrate_forces(state):
 	if Input.is_action_pressed("character_move_right"):
 		f.x += SPEED
 		isBlend = true
-		#anim_player.play("Crawl")
+		anim_player.play("Crawl")
 	elif Input.is_action_pressed("character_move_left"):
 		f.x -= SPEED
 		isBlend = true
-		#anim_player.play("Crawl")
+		anim_player.play("Crawl")
 		
 	elif Input.is_action_just_pressed("character_move_up"):
 		f.y -= JUMP_SPEED
 		isBlend = true
-		#anim_player.play("Idle")
+		anim_player.play("Idle")
 	else:
 		#anim_player.play("Run")
-		#anim_player.play("Idle")
+		anim_player.play("Idle")
 		#anim_player.stop()
-		#isBlend = false
-		pass
+		isBlend = true
 	
-	apply_impulse(Vector2(), f)
+	apply_impulse(Vector2(0, 0), f)
 

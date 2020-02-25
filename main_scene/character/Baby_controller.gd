95c3d57c3aac13995e82929a239e856f5b78f787
cccccroge <pooopooo543@gmail.com>
2020-02-25 16:10:15 +0800

    Success on flip, should fix pinjoints

--- main_scene/character/Baby_controller.gd
+++ main_scene/character/Baby_controller.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 # Movement
-var isMoving := false		# give hint to two rigid base
+var isMoving := false		# give hint to script of two rigid base
 
 # Active ragdoll
 export var anim_ratio = 0.15
@@ -20,28 +20,17 @@ var bone2rigid_rot_list_l := []
 var bone2rigid_rot_list_target
 
 # Angle constraints
-export(int) var head_min = -26
-export(int) var head_max = 32
-
-export(int) var armUpperLeft_min = -70
-export(int) var armUpperLeft_max = 90
-export(int) var armLowerLeft_min = -80
-export(int) var armLowerLeft_max = 25
-export(int) var armUpperRight_min = -80
-export(int) var armUpperRight_max = 80
-export(int) var armLowerRight_min = -68
-export(int) var armLowerRight_max = 45
-
-export(int) var legUpperLeft_min = -75
-export(int) var legUpperLeft_max = 80
-export(int) var legLowerLeft_min = -80
-export(int) var legLowerLeft_max = 48
-export(int) var legUpperRight_min = -15
-export(int) var legUpperRight_max = 130
-export(int) var legLowerRight_min = -130
-export(int) var legLowerRight_max = 2
-
-var rigid_angle_constraints := []
+export(Array, Array, int) var rigid_angle_constraints := [
+	[0, 0], [-26, 32], # Base & Head, Base's angles are meaningless, just for alignment
+	[-70, 90], [-80, 25], [-80, 80], [-68, 45], # Arm left/right
+	[-75, 80], [-80, 48], [-15, 130], [-130, 2]	# Leg left/right
+]
+export(Array, Array, int) var rigid_angle_constraints_l := [
+	[0, 0], [-40, 16], # Base & Head, Base's angles are meaningless, just for alignment
+	[-55, 90], [-40, 75], [-77, 75], [-50, 72], # Arm left/right
+	[-90, 63], [-53, 70], [-70, 80], [-77, 50]	# Leg left/right
+]
+var rigid_angle_constraints_target
 
 # Character flip
 var isFlip := false		# default toward right
@@ -89,37 +78,47 @@ func _ready():
 	
 	assert(bone_list.size() == rigid_list_l.size())
 	for i in range(bone_list.size()):
+		var bone_rot = PI - bone_list[i].get_rotation()	# flip the angle cuz 
+														# scaling skeleton won't affect bone's angle
 		bone2rigid_rot_list_l.append(rigid_list_l[i].get_rotation()\
-			 - bone_list[i].get_rotation())
+			 - bone_rot)
 	
-	# Get angle constraints (should be same order as the other lists)
-	rigid_angle_constraints.append([0, 0])	# for Base, meaningless, just for alignment
-	rigid_angle_constraints.append([deg2rad(head_min), deg2rad(head_max)])
-	rigid_angle_constraints.append([deg2rad(armUpperLeft_min), deg2rad(armUpperLeft_max)])
-	rigid_angle_constraints.append([deg2rad(armUpperRight_min), deg2rad(armUpperRight_max)])
-	rigid_angle_constraints.append([deg2rad(legUpperLeft_min), deg2rad(legUpperLeft_max)])
-	rigid_angle_constraints.append([deg2rad(legUpperRight_min), deg2rad(legUpperRight_max)])
-	rigid_angle_constraints.append([deg2rad(armLowerLeft_min), deg2rad(armLowerLeft_max)])
-	rigid_angle_constraints.append([deg2rad(armLowerRight_min), deg2rad(armLowerRight_max)])
-	rigid_angle_constraints.append([deg2rad(legLowerLeft_min), deg2rad(legLowerLeft_max)])
-	rigid_angle_constraints.append([deg2rad(legLowerRight_min), deg2rad(legLowerRight_max)])
+	# Get all angle constraints in radians
+	for list in rigid_angle_constraints:
+		list[0] = deg2rad(list[0])
+		list[1] = deg2rad(list[1])
+	
+	for list in rigid_angle_constraints_l:
+		list[0] = deg2rad(list[0])
+		list[1] = deg2rad(list[1])
 
 	# Set default flip to right
 	flip("right")
+
 
 func _physics_process(delta):
 	if isBlend:
 		# blend rigid's rotation with animation
 		for i in range(0, bone_list.size()-4):
 			var physics = rigid_list_target[i].get_rotation()
-			var animation = bone_list[i].get_rotation() + bone2rigid_rot_list_target[i]
+			
+			var bone_rot = bone_list[i].get_rotation()
+			if isFlip:
+				bone_rot = PI - bone_rot
+			var animation = bone_rot + bone2rigid_rot_list_target[i]
+			
 			rigid_list_target[i].set_rotation(
 				physics * physics_ratio + animation * anim_ratio)
 	
 	# blend feet anyway to look more realistic
 	for i in range(bone_list.size()-4, bone_list.size()):
 			var physics = rigid_list_target[i].get_rotation()
-			var animation = bone_list[i].get_rotation() + bone2rigid_rot_list_target[i]
+			
+			var bone_rot = bone_list[i].get_rotation()
+			if isFlip:
+				bone_rot = PI - bone_rot
+			var animation = bone_rot + bone2rigid_rot_list_target[i]
+			
 			rigid_list_target[i].set_rotation(
 				physics * physics_ratio + animation * anim_ratio)
 			
@@ -127,7 +126,8 @@ func _physics_process(delta):
 	for i in range(1, rigid_list_target.size()):
 		rigid_list_target[i].set_rotation(
 			clamp(rigid_list_target[i].get_rotation(), 
-				  rigid_angle_constraints[i][0], rigid_angle_constraints[i][1]))
+				  rigid_angle_constraints_target[i][0], rigid_angle_constraints_target[i][1]))
+
 
 func _process(delta):
 	# Do animation
@@ -158,25 +158,47 @@ func _process(delta):
 		isMoving = false
 		anim_player.play("Idle")
 
+
+""" Swapping two tree of rigid bodies, achieving 
+	fliping sprites & collision shapes """
+
 func flip(direction):
 	if direction == "left":
+		# init position (must do it before set it back to rigid mode!)
 		rigid_list_l[0].set_position(rigid_list[0].get_position())
-		
+		# swap physics simulation
 		set_all_rigid(rigid_list, false)
 		set_all_rigid(rigid_list_l, true)
-		
+		# swap visibilities
+		rigid_list[0].set_visible(false)
+		rigid_list_l[0].set_visible(true)
+		# set blending target of active ragdoll
 		rigid_list_target = rigid_list_l
 		bone2rigid_rot_list_target = bone2rigid_rot_list_l
+		rigid_angle_constraints_target = rigid_angle_constraints_l
+		# flip animation
+		skeleton.set_scale(Vector2(-1, 1))
+		
 	elif direction == "right":
 		rigid_list[0].set_position(rigid_list_l[0].get_position())
 		
 		set_all_rigid(rigid_list_l, false)
 		set_all_rigid(rigid_list, true)
 		
+		rigid_list_l[0].set_visible(false)
+		rigid_list[0].set_visible(true)
+		
 		rigid_list_target = rigid_list
 		bone2rigid_rot_list_target = bone2rigid_rot_list
+		rigid_angle_constraints_target = rigid_angle_constraints
+		
+		skeleton.set_scale(Vector2(1, 1))
+
+
+""" Set mode of all rigid bodies in the list to RIGID/disabled STATIC """
 
 func set_all_rigid(list, boolean):
+	var mode = RigidBody2D.MODE_RIGID if boolean else RigidBody2D.MODE_STATIC
 	for rigid in list:
-		rigid.set_mode(RigidBody2D.MODE_RIGID if boolean 
-			else RigidBody2D.MODE_STATIC)
+		rigid.set_mode(mode)
+		rigid.set_collision_mask_bit(3, boolean)
